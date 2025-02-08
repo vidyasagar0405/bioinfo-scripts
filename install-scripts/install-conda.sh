1dd197c6afa9256adeb077f2084ec1e1e8a28604
vidyasagar0405 <vidyasagar0405@gmail.com>
2025-02-08 13:33:17 +0530

    fix(files): reorganised

---
+++ install-scripts/install-conda.sh
@@ -0,0 +1,11 @@
+echo "Removing existing miniconda"
+rm -rf ~/miniconda3/
+echo "Installing Miniconda..."
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda init --all
+conda config --set changeps1 false
+echo "Miniconda installed successfully."
